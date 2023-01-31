42b436817ddb25666e52876f03adbbb19b40275d
Diego Oliveira <diego.fernandes@gmail.com>
2023-01-31 19:08:20 -0300

    Feito o background e btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-window.onload = function () {
-    document.querySelector(".menu_mobile").addEventListener("click", function () {
-        if(document.querySelector(".menu nav ul").style.display == 'flex') {
-            document.querySelector(".menu nav ul").style.display = 'none';
+window.onload = () => {
+    document.querySelector('.menu_mobile').addEventListener('click', () => {
+        if(document.querySelector('.menu nav ul').style.display === 'flex') {
+            document.querySelector('.menu nav ul').style.display = 'none';
         }else {
-            document.querySelector(".menu nav ul").style.display = 'flex';
+            document.querySelector('.menu nav ul').style.display = 'flex';
         }
     });
 }
