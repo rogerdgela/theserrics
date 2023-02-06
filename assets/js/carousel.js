198491556c382b3671354c987a700cb214bb6115
douglas bergamo <douglasdeoliveirabergamo@gmail.com>
2023-02-06 19:24:59 -0300

    mudança de parceiros para passar as imagens 1 por vez

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -8,7 +8,7 @@ $('.carousel').slick({
             breakpoint: 1024,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 4,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
@@ -17,7 +17,7 @@ $('.carousel').slick({
             breakpoint: 769,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1
             }
         },
 
